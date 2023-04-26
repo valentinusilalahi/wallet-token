6329d7ad5f9a160a4fc8a02803febc1bbeb42f8b
Valentinus Silalahi <valentinus@doku.com>
2023-04-26 14:15:50 +0700

    first commit

---
+++ src/main/java/com/silalahi/valentinus/training/WalletTokenApplication.java
@@ -0,0 +1,13 @@
+package com.silalahi.valentinus.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletTokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silalahi/valentinus/training/WalletTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silalahi.valentinus.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
